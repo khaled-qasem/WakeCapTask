183acb9711679407d06e5cfa6034604149d4e74b
Khaled Qasem <khaled.alqerem@gmail.com>
2019-01-14 22:00:12 +0200

    Implement utils classes required for recycler view

---
+++ app/src/main/java/com/khaled/wakecap/wakecaptask/util/SimpleDividerItemDecoration.kt
@@ -0,0 +1,30 @@
+package com.khaled.wakecap.wakecaptask.util
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.drawable.Drawable
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.khaled.wakecap.wakecaptask.R
+
+class SimpleDividerItemDecoration(context: Context) : RecyclerView.ItemDecoration() {
+    private val divider: Drawable? = ContextCompat.getDrawable(context, R.drawable.line_divider)
+
+    override fun onDrawOver(canvas: Canvas, parent: RecyclerView, state: RecyclerView.State) {
+        val left = parent.paddingStart
+        val right = parent.width - parent.paddingEnd
+
+        val childCount = parent.childCount
+        for (i in 0 until childCount) {
+            val child = parent.getChildAt(i)
+
+            val params = child.layoutParams as RecyclerView.LayoutParams
+
+            val top = child.bottom + params.bottomMargin
+            val bottom = top + ((divider?.intrinsicHeight) ?: 0)
+
+            divider?.setBounds(left, top, right, bottom)
+            divider?.draw(canvas)
+        }
+    }
+}
